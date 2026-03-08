08a4652d4fce74666f53db6ab2a95e5d2d61f0f8
Andhmu <andranikmur@hotmail.com>
2026-03-08 19:51:33 +0300

    css commit

---
+++ src copy 3/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+// import './assets/mobile.css'
+// import './assets/desctop.css'
+
+const app = createApp(App);
+app.use(createPinia());
+app.mount('#app');
